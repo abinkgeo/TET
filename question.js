20de4c26c07e47a079a131274deaf29dfb5612e1
abinkgeo <146973696+abinkgeo@users.noreply.github.com>
2024-04-09 10:52:50 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,565 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which solid structure has a definite and sharp melting point?",
+        answer: "D. Crystalline solids",
+        options: [
+            "A. All types of solids ",
+            "B. No type of solid",
+            "C. Amorphous solids	",
+            "D. Crystalline solids"
+        ]
+    },
+    {
+        numb: 2,
+        question: "______ is a crystalline solid’s basic repetitive structural unit.",
+        answer: "C. Unit cell",
+        options: [
+            "A.Monomer	",
+            "B. Molecule",
+            "C. Unit cell",
+            "D. Atom"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Sulfur exists in two polymorphic forms _________ and _________",
+        answer: "A. rhombic and monoclinic",
+        options: [
+            "A. rhombic and monoclinic",
+            "B. rhombic and triclinic",
+            "C. hexagonal and triclinic",
+            "D. hexagonal and monoclinic"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the total number of Bravais lattices in the crystal structures?",
+        answer: "C.14",
+        options: [
+            "A. 3	",
+            "B.6",
+            "C.14",
+            "D. 24"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following claims about crystal lattice diffraction patterns is correct?",
+        answer: "A. Diffraction patterns possess a centre of symmetry",
+        options: [
+            "A. Diffraction patterns possess a centre of symmetry",
+            "B. Diffraction patterns don’t have centre of symmetry",
+            "C. Diffraction patterns are linear space",
+            "D. Diffraction patterns contains α-rays"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "The volume occupied by a single gas in a mixture at the same temperature and pressure is referred to as the single-gas volume.",
+        answer: "A. Absolute volume",
+        options: [
+            "A. Absolute volume",
+            "B. Partial volume",
+            "C. Total volume of a gas mixtureg",
+            "D. None of the above"
+        ]
+    },
+
+    {
+        numb:7,
+        question: "When two perfect solutions with volume V each are combined, What is the volume of the solution as a result?",
+        answer: "B. 2V",
+        options: [
+            "A. V",
+            "B. 2V",
+            "C. Greater than 2V",
+            "D. Less than 2V"
+        ]
+
+    },
+
+    {
+        numb:8,
+        question: "What is an example of camphor in N2 gas?",
+        answer: "A. Solid in gas solution",
+        options: [
+            "A. Solid in gas solution",
+            "B. Gas in gas solution",
+            "C. Solid in liquid solution",
+            "D. Liquid in gas solution"
+        ]
+
+    },
+
+    {
+        numb:9,
+        question: "What happens when a solute crystal is added to a supersaturated solution?",
+        answer: "D. The solute precipitates out of the solution",
+        options: [
+            "A. It becomes a colloidal solution",
+            "B. The solute dissolves in the solution",
+            "C. The solution desaturates",
+            "D. The solute precipitates out of the solution"
+        ]
+
+    },
+
+    {
+        numb:10,
+        question: "The solute precipitates out of the solution",
+        answer: "A. Solute and solvent",
+        options: [
+            "A. Solute and solvent",
+            "B. Solute and solute",
+            "C.Solvent and solvent	",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb:11,
+        question: "A cathode and an anode are the most common components of an electrochemical cell. Which of the following claims about the cathode is correct?",
+        answer: "B. Electrons move into the cathode",
+        options: [
+            "A. Oxidation occurs at the cathode		",
+            "B. Electrons move into the cathode",
+            "C.Solvent and solventUsually denoted by a negative sign",
+            "D. Is usually made up of insulating material"
+        ]
+    },
+    {
+        numb:12,
+        question: "Which of the following does not belong in the category of electrochemical cells?",
+        answer: "B. Photovoltaic cell",
+        options: [
+            "A. Voltaic cell		",
+            "B. Photovoltaic cell",
+            "C.Electrolytic cell	",
+            "D. Fuel Cell"
+        ]
+    },
+    {
+        numb:13,
+        question: "Which of the following assertions about the main cell is correct?",
+        answer: "A. An example of a primary cell is a mercury cell",
+        options: [
+            "A. An example of a primary cell is a mercury cell",
+            "B. An example of a primary cell is a nickel-cadmium storage cell",
+            "C.The electrode reactions can be reversed",
+            "D. It can be recharged"
+        ]
+    },
+    {
+        numb:14,
+        question: "In a dry cell, which of the following is the electrolyte?",
+        answer: "C.Ammonium chloride",
+        options: [
+            "A. Potassium hydroxide			",
+            "B.Sulphuric acid",
+            "C.Ammonium chloride",
+            "D. Manganese dioxide"
+        ]
+    },
+    {
+        numb:15,
+        question: "Which of the following statements about a lead storage cell (or a lead-acid battery) is false?",
+        answer: "A.It is a primary cell	",
+        options: [
+            "A.It is a primary cell",
+            "B. The cathode is made up of lead(IV) oxide",
+            "C.The anode is made up of lead",
+            "D. The electrolyte used is an aqueous solution of sulphuric acid"
+        ]
+    },
+{
+    "numb": 16,
+    "question": "Which charge configuration produces a uniform electric field?",
+    "answer": "C. uniformly charged infinite plane",
+    "options": [
+        "A. point charge",
+        "B. uniformly charged infinite line",
+        "C. uniformly charged infinite plane",
+        "D. uniformly charged spherical shell"
+    ]
+},
+{
+    "numb": 17,
+    "question": "An electric dipole is placed at an alignment angle of 30° with an electric field of 2 × 10^5 N C^−1. It experiences a torque equal to 8 N m. The charge on the dipole if the dipole length is 1 cm is",
+    "answer": "(c) 5 mC",
+    "options": [
+        "(a) 4 mC",
+        "(b) 8 mC",
+        "(c) 5 mC",
+        "(d) 7 mC"
+    ]
+},
+{
+    "numb": 18,
+    "question": "Two identical conducting balls having positive charges q1 and q2 are separated by a centre to centre distance r. If they are made to touch each other and then separated to the same distance, the force between them will be",
+    "answer": "(b) same as before",
+    "options": [
+        "(a) less than before",
+        "(b) same as before",
+        "(c) more than before",
+        "(d) zero"
+    ]
+},
+{
+    "numb": 19,
+    "question": "Two points A and B are maintained at a potential of 7 V and -4 V respectively. The work done in moving 50 electrons from A to B is",
+    "answer": "(b) -8.80 × 10^−17 J",
+    "options": [
+        "(a) 8.80 × 10^−17 J",
+        "(b) -8.80 × 10^−17 J",
+        "(c) 4.40 × 10^−17 J",
+        "(d) 5.80 × 10^−17 J"
+    ]
+},
+{
+    "numb": 20,
+    "question": "A parallel plate capacitor stores a charge Q at a voltage V. Suppose the area of the parallel plate capacitor and the distance between the plates are each doubled then which is the quantity that will change?",
+    "answer": "(a) Capacitance",
+    "options": [
+        "(a) Capacitance",
+        "(b) Charge",
+        "(c) Voltage",
+        "(d) Energy density"
+    ]
+},
+{
+    "numb": 21,
+    "question": "One of the combinations from the fundamental physical constants is hc/G. The unit of this expression is",
+    "answer": "(c) s^−1",
+    "options": [
+        "(a) kg^2",
+        "(b) m^3",
+        "(c) s^−1",
+        "(d) m"
+    ]
+},
+{
+    "numb": 22,
+    "question": "The length of a body is measured as 3.51 m, if the accuracy is 0.01m, then the percentage error in the measurement is",
+    "answer": "(c) 0.28%",
+    "options": [
+        "(a) 351%",
+        "(b) 1%",
+        "(c) 0.28%",
+        "(d) 0.035%"
+    ]
+},
+{
+    "numb": 23,
+    "question": "If π = 3.14, then the value of π^2 is",
+    "answer": "(b) 9.860",
+    "options": [
+        "(a) 9.8596",
+        "(b) 9.860",
+        "(c) 9.86",
+        "(d) 9.9"
+    ]
+},
+{
+    "numb": 24,
+    "question": "Which of the following pairs of physical quantities have the same dimension?",
+    "answer": "d) force and torque",
+    "options": [
+        "a) force and power",
+        "b) torque and energy",
+        "c) torque and power",
+        "d) force and torque"
+    ]
+},
+{
+    "numb": 25,
+    "question": "The dimension of (μ0 ε0)^−1/2 is",
+    "answer": "(a) length",
+    "options": [
+        "(a) length",
+        "(b) time",
+        "(c) velocity",
+        "(d) force"
+    ]
+},
+{
+    "numb": 26,
+    "question": "Which one of the following physical quantities cannot be represented by a scalar?",
+    "answer": "(c) momentum",
+    "options": [
+        "(a) Mass",
+        "(b) length",
+        "(c) momentum",
+        "(d) magnitude of acceleration"
+    ]
+},
+{
+    "numb": 27,
+    "question": "A toaster operating at 240 V has a resistance of 120 Ω. Its power is",
+    "answer": "(c) 480 W",
+    "options": [
+        "(a) 400 W",
+        "(b) 2 W",
+        "(c) 480 W",
+        "(d) 240 W"
+    ]
+},
+{
+    "numb": 28,
+    "question": "A carbon resistor of (47 ± 4.7 ) k Ω to be marked with rings of different colours for its identification. The colour code sequence will be",
+    "answer": "b) Yellow – Violet – Orange – Silver",
+    "options": [
+        "a) Yellow – Green – Violet – Gold",
+        "b) Yellow – Violet – Orange – Silver",
+        "c) Violet – Yellow – Orange – Silver",
+        "d) Green – Orange – Violet – Gold"
+    ]
+},
+{
+    "numb": 29,
+    "question": "Two wires of A and B with circular cross-section are made up of the same material with equal lengths. Suppose RA = 3 RB, then what is the ratio of the radius of wire A to that of B?",
+    "answer": "a) 3",
+    "options": [
+        "a) 3",
+        "b) √3",
+        "c) 1 /√3",
+        "d) 1 /3"
+    ]
+},
+{
+    "numb": 30,
+    "question": "A wire connected to a power supply of 230 V has power dissipation P1. Suppose the wire is cut into two equal pieces and connected parallel to the same power supply. In this case, power dissipation is P2. The ratio P2/p1 is",
+    "answer": "(b) 2",
+    "options": [
+        "(a) 1",
+        "(b) 2",
+        "(c) 3",
+        "(d) 4"
+    ]
+},
+{
+    "numb": 31,
+    "question": "A relation R in a set A is called ________, if (a1, a2) ∈ R implies (a2, a1) ∈ R, for all a1, a2 ∈ A.",
+    "answer": "(a) symmetric",
+    "options": [
+        "(a) symmetric",
+        "(b) transitive",
+        "(c) equivalence",
+        "(d) non-symmetric"
+    ]
+},
+{
+    "numb": 32,
+    "question": "Let R be a relation on the set N of natural numbers defined by nRm if n divides m. Then R is",
+    "answer": "(d) Reflexive, transitive but not symmetric",
+    "options": [
+        "(a) Reflexive and symmetric",
+        "(b) Transitive and symmetric",
+        "(c) Equivalence",
+        "(d) Reflexive, transitive but not symmetric"
+    ]
+},
+{
+    "numb": 33,
+    "question": "The maximum number of equivalence relations on the set A = {1, 2, 3} are",
+    "answer": "(d) 5",
+    "options": [
+        "(a) 1",
+        "(b) 2",
+        "(c) 3",
+        "(d) 5"
+    ]
+},
+{
+    "numb": 34,
+    "question": "If set A contains 5 elements and the set B contains 6 elements, then the number of one-one and onto mappings from A to B is",
+    "answer": "(c) 0",
+    "options": [
+        "(a) 720",
+        "(b) 120",
+        "(c) 0",
+        "(d) none of these"
+    ]
+},
+{
+    "numb": 35,
+    "question": "Let f : [2, ∞) → R be the function defined by f(x) = x^2 – 4x + 5, then the range of f is",
+    "answer": "(b) [1, ∞)",
+    "options": [
+        "(a) R",
+        "(b) [1, ∞)",
+        "(c) [4, ∞)",
+        "(d) [5, ∞)"
+    ]
+},
+{
+    "numb": 36,
+    "question": "Let f : R → R be defined by f(x) = 1/x ∀ x ∈ R. Then f is",
+    "answer": "(d) f is not defined",
+    "options": [
+        "(a) one-one",
+        "(b) onto",
+        "(c) bijective",
+        "(d) f is not defined"
+    ]
+},
+{
+    "numb": 37,
+    "question": "Let A = {1, 2, 3} and consider the relation R = {(1, 1), (2, 2), (3, 3), (1, 2), (2, 3), (1,3)}. Then R is",
+    "answer": "(a) reflexive but not symmetric",
+    "options": [
+        "(a) reflexive but not symmetric",
+        "(b) reflexive but not transitive",
+        "(c) symmetric and transitive",
+        "(d) neither symmetric, nor transitive"
+    ]
+},
+{
+    "numb": 38,
+    "question": "If f : R → R be defined by f(x) = 3x^2 – 5 and g : R → R by g(x) = x/(x^2 + 1), then g o f is",
+    "answer": "(a) (3x^2 – 5)/(9x^4 – 30x^2 + 26)",
+    "options": [
+        "(a) (3x^2 – 5)/(9x^4 – 30x^2 + 26)",
+        "(b) (3x^2 – 5)/(9x^4 – 6x^2 + 26)",
+        "(c) 3x^2/(x^4 + 2x^2 – 4)",
+        "(d) 3x^2/(9x^4 + 30x^2 – 2)"
+    ]
+},
+{
+    "numb": 39,
+    "question": "Let f : R → R be given by f (x) = tan x. Then f^−1(1) is",
+    "answer": "(a) π/4",
+    "options": [
+        "(a) π/4",
+        "(b) {n π + π/4 : n ∈ Z}",
+        "(c) does not exist",
+        "(d) none of these"
+    ]
+},
+{
+    "numb": 40,
+    "question": "If f: R → R be given by f(x) = (3 – x^3)^(1/3), then fof(x) is",
+    "answer": "(c) x",
+    "options": [
+        "(a) x^(1/3)",
+        "(b) x^3",
+        "(c) x",
+        "(d) (3 – x^3)"
+    ]
+},
+{
+    "numb": 41,
+    "question": "Let f : A → B and g : B → C be the bijective functions. Then (g ∘ f)^−1 is",
+    "answer": "(a) f^−1 ∘ g^−1",
+    "options": [
+        "(a) f^−1 ∘ g^−1",
+        "(b) f ∘ g",
+        "(c) g^−1 ∘ f^−1",
+        "(d) g ∘ f"
+    ]
+},
+{
+    "numb": 42,
+    "question": "Consider a binary operation ∗ on N defined as a ∗ b = a^3 + b^3. Choose the correct answer.",
+    "answer": "(b) ∗ is commutative but not associative",
+    "options": [
+        "(a) ∗ is both associative and commutative",
+        "(b) ∗ is commutative but not associative",
+        "(c) ∗ is associative but not commutative",
+        "(d) ∗ is neither commutative nor associative"
+    ]
+},
+{
+    "numb": 43,
+    "question": "Let f : R → R be defined by f(x) = x^2 + 1. Then, pre-images of 17 and –3, respectively, are",
+    "answer": "(c) {4, –4}, φ",
+    "options": [
+        "(a) φ, {4, –4}",
+        "(b) {3, –3}, φ",
+        "(c) {4, –4}, φ",
+        "(d) {4, –4}, {2, –2}"
+    ]
+},
+{
+    "numb": 44,
+    "question": "Set A has 3 elements, and set B has 4 elements. Then the number of injective mappings that can be defined from A to B is",
+    "answer": "(c) 24",
+    "options": [
+        "(a) 144",
+        "(b) 12",
+        "(c) 24",
+        "(d) 64"
+    ]
+},
+{
+    "numb": 45,
+    "question": "Let f : R → R be defined by f(x) = 3x – 4. Then f^−1(x) is given by",
+    "answer": "(a) (x + 4)/3",
+    "options": [
+        "(a) (x + 4)/3",
+        "(b) (x/3) – 4",
+        "(c) 3x + 4",
+        "(d) None of these"
+    ]
+},
+{
+    "numb": 46,
+    "question": "The identity element for the binary operation * defined on Q ~ {0} as a * b = ab/2 ∀ a, b ∈ Q ~ {0} is",
+    "answer": "(c) 2",
+    "options": [
+        "(a) 1",
+        "(b) 0",
+        "(c) 2",
+        "(d) none of these"
+    ]
+},
+{
+    "numb": 47,
+    "question": "Number of binary operations on the set {a, b} are",
+    "answer": "(b) 16",
+    "options": [
+        "(a) 10",
+        "(b) 16",
+        "(c) 20",
+        "(d) 8"
+    ]
+},
+{
+    "numb": 48,
+    "question": "Let A = {1, 2, 3}. Then the number of relations containing (1, 2) and (1, 3), which are reflexive and symmetric but not transitive is",
+    "answer": "(a) 1",
+    "options": [
+        "(a) 1",
+        "(b) 2",
+        "(c) 3",
+        "(d) 4"
+    ]
+},
+{
+    "numb": 49,
+    "question": "Let f , g : R → R be defined by f(x) = 3x + 1 and g(x) = x^2 – 2, ∀ x ∈ R, respectively. Then, f og is",
+    "answer": "(b) 3x^2 – 5",
+    "options": [
+        "(a) 9x^2 + 6x – 1",
+        "(b) 3x^2 – 5",
+        "(c) 9x^2 – 6x – 3",
+        "(d) 3x^2"
+    ]
+},
+{
+    "numb": 50,
+    "question": "The principal value of tan^−1(tan(3π/5)) is",
+    "answer": "(b) -2π/5",
+    "options": [
+        "(a) 2π/5",
+        "(b) -2π/5",
+        "(c) 3π/5",
+        "(d) -3π/5"
+    ]
+}
+    
+
+
+
+
+]
